a1630a2ad999d9f067915f5f58ac7737cb809300
Christian Batista <Christian88batista@gmail.com>
2022-01-12 21:45:18 -0500

    first commit

--- src/edu/mtc/egr283/ShoppingList/SLLNode.java
+++ src/edu/mtc/egr283/ShoppingList/SLLNode.java
@@ -4,14 +4,14 @@ public class SLLNode<T> {
 
 	private T nodeData;
 	private SLLNode<T> next;
-	
+
 	/**
 	 * Default constructor
 	 */
 	public SLLNode() {
 		this(null, null);
 	}// Ending bracket of new Constructor
-	
+
 	/**
 	 * @param newNodeData
 	 * @param newNext
@@ -27,26 +27,26 @@ public class SLLNode<T> {
 	public T getNodeData() {
 		return this.nodeData;
 	}// Ending bracket of method getNodeData
-	
+
 	/**
 	 * @param nodeData the nodeData to set
 	 */
 	public void setNodeData(T newNodeData) {
 		this.nodeData = newNodeData;
 	}// Ending bracket of method setNodeData
-	
+
 	/**
 	 * @return the next
 	 */
 	public SLLNode<T> getNext() {
 		return this.next;
 	}// Ending bracket of method getNext
-	
+
 	/**
 	 * @param newNext the next to set
 	 */
 	public void setNext(SLLNode<T> newNext) {
 		this.next = newNext;
 	}
-	
+
 }// Ending bracket of class SLLNode
